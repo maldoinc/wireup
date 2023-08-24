454776276214bcd9edce89269c8d6ff493077f1a
Aldo Mateli <aldo.mateli@gmail.com>
2023-08-24 22:07:12 +0100

    Add docstring

--- wireup/__init__.py
+++ wireup/__init__.py
@@ -2,3 +2,4 @@ from wireup.ioc.dependency_container import DependencyContainer
 from wireup.ioc.parameter import ParameterBag
 
 container = DependencyContainer(ParameterBag())
+"""Singleton DI container initialized for convenience"""
