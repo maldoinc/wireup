a3fa492e5547480638d3137e9be4c287e5903728
Aldo Mateli <aldo.mateli@gmail.com>
2023-08-25 22:41:40 +0100

    Merge param test cases

--- test/test_parameter.py
+++ test/test_parameter.py
@@ -4,61 +4,45 @@ from wireup.ioc.container_util import ParameterWrapper
 from wireup.ioc.parameter import ParameterBag, TemplatedString
 
 
-class TestTemplatedString(unittest.TestCase):
-    def test_init(self):
-        val = "test value"
-        templated_str = TemplatedString(val)
-        self.assertEqual(templated_str.value, val)
-
-
 class TestParameterBag(unittest.TestCase):
-    def test_put_get(self):
-        param_bag = ParameterBag()
-        param_bag.put("param1", 42)
+    def setUp(self) -> None:
+        self.bag = ParameterBag()
 
-        self.assertEqual(param_bag.get("param1"), 42)
+    def test_put_get(self):
+        self.bag.put("param1", 42)
+
+        self.assertEqual(self.bag.get("param1"), 42)
 
     def test_put_get_templated_string_is_converted(self):
-        param_bag = ParameterBag()
-        param_bag.put("param1", 42)
+        self.bag.put("param1", 42)
         templated_str = TemplatedString("Number: ${param1}")
 
-        self.assertEqual(param_bag.get(templated_str), "Number: 42")
-        self.assertEqual(param_bag.get(TemplatedString("${param1}")), "42")
+        self.assertEqual(self.bag.get(templated_str), "Number: 42")
+        self.assertEqual(self.bag.get(TemplatedString("${param1}")), "42")
 
     def test_get_unknown_parameter(self):
-        param_bag = ParameterBag()
-
         with self.assertRaises(ValueError):
-            param_bag.get("unknown_param")
+            self.bag.get("unknown_param")
 
-    def test_update(self):
-        param_bag = ParameterBag()
-        param_bag.put("param1", 42)
-        param_bag.update({"param2": "value"})
+    def test_update_assert_merges(self):
+        self.bag.put("param1", 42)
+        self.bag.put("param2", "foo")
+        self.bag.update({"param2": "value"})
 
-        self.assertEqual(param_bag.get("param2"), "value")
-
-    def test_interpolate(self):
-        param_bag = ParameterBag()
-        param_bag.put("param1", "value")
-        templated_str = TemplatedString("Test ${param1}")
-
-        self.assertEqual(param_bag.get(templated_str), "Test value")
+        self.assertEqual(self.bag.get("param1"), 42)
+        self.assertEqual(self.bag.get("param2"), "value")
 
     def test_interpolate_unknown_parameter(self):
-        param_bag = ParameterBag()
         templated_str = TemplatedString("Test ${unknown_param}")
 
         with self.assertRaises(ValueError):
-            param_bag.get(templated_str)
+            self.bag.get(templated_str)
 
     def test_all(self):
-        bag = ParameterBag()
-        bag.put("foo", "bar")
-        bag.update({"bar": "baz", "baz": "qux"})
+        self.bag.put("foo", "bar")
+        self.bag.update({"bar": "baz", "baz": "qux"})
 
-        self.assertEqual(bag.get_all(), {"foo": "bar", "bar": "baz", "baz": "qux"})
+        self.assertEqual(self.bag.get_all(), {"foo": "bar", "bar": "baz", "baz": "qux"})
 
 
 class TestParameterPlaceholder(unittest.TestCase):
@@ -67,3 +51,10 @@ class TestParameterPlaceholder(unittest.TestCase):
         placeholder = ParameterWrapper(param_ref)
 
         self.assertEqual(placeholder.param, param_ref)
+
+
+class TestTemplatedString(unittest.TestCase):
+    def test_init(self):
+        val = "test value"
+        templated_str = TemplatedString(val)
+        self.assertEqual(templated_str.value, val)
