11551b578f4dccfd9470d7329149ceec07ccd03a
Aldo Mateli <aldo.mateli@gmail.com>
2023-11-19 15:47:27 +0000

    Reorganize flask integration tests

--- test/integration/test_flask_integration.py
+++ test/integration/test_flask_integration.py
@@ -7,10 +7,6 @@ from wireup import DependencyContainer, ParameterBag, Wire
 from wireup.integration.flask_integration import wireup_init_flask_integration
 
 
-def will_not_get_autowired():
-    return "not autowired"
-
-
 class TestFlaskIntegration(unittest.TestCase):
     def setUp(self):
         self.container = DependencyContainer(ParameterBag())
@@ -19,20 +15,13 @@ class TestFlaskIntegration(unittest.TestCase):
         self.app = Flask(__name__)
         self.app.config["TESTING"] = True
 
+        self.client = self.app.test_client()
+
+    def test_get_random_is_autowired_only_from_type(self):
         @self.app.get("/random")
         def get_random(random: RandomService):
             return {"lucky_number": random.get_random()}
 
-        @self.app.get("/env")
-        def get_environment(
-            is_debug: Annotated[bool, Wire(param="DEBUG")], is_test: Annotated[bool, Wire(param="TESTING")]
-        ):
-            return {"debug": is_debug, "test": is_test}
-
-        self.app.get("/not-autowired")(will_not_get_autowired)
-        self.client = self.app.test_client()
-
-    def test_get_random_is_autowired_only_from_type(self):
         wireup_init_flask_integration(self.app, dependency_container=self.container, service_modules=[])
         res = self.client.get("/random")
 
@@ -40,6 +29,12 @@ class TestFlaskIntegration(unittest.TestCase):
         self.assertEqual(res.json, {"lucky_number": 4})
 
     def test_get_env_injects_from_params(self):
+        @self.app.get("/env")
+        def get_environment(
+            is_debug: Annotated[bool, Wire(param="DEBUG")], is_test: Annotated[bool, Wire(param="TESTING")]
+        ):
+            return {"debug": is_debug, "test": is_test}
+
         wireup_init_flask_integration(self.app, dependency_container=self.container, service_modules=[])
         res = self.client.get("/env")
 
@@ -47,6 +42,10 @@ class TestFlaskIntegration(unittest.TestCase):
         self.assertEqual(res.json, {"debug": False, "test": True})
 
     def test_will_not_autowire_when_no_injections_requested(self):
+        @self.app.get("/not-autowired")
+        def will_not_get_autowired():
+            return "not autowired"
+
         wireup_init_flask_integration(self.app, dependency_container=self.container, service_modules=[])
         res = self.client.get("/not-autowired")
 
