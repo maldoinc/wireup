6ae829222d39d95e25e25e553405bca1acbd2573
Aldo Mateli <aldo.mateli@gmail.com>
2023-12-22 21:11:16 +0000

    Rm unnecessary check

--- wireup/integration/util.py
+++ wireup/integration/util.py
@@ -8,12 +8,11 @@ from wireup.ioc.util import parameter_get_type_and_annotation
 
 def is_view_using_container(dependency_container: DependencyContainer, view: Callable[..., Any]) -> bool:
     """Determine whether the view is using the given dependency container."""
-    if hasattr(view, "__annotations__"):
-        for dep in inspect.signature(view).parameters.values():
-            param = parameter_get_type_and_annotation(dep)
+    for dep in inspect.signature(view).parameters.values():
+        param = parameter_get_type_and_annotation(dep)
 
-            is_requesting_injection = isinstance(param.annotation, InjectableType)
-            if is_requesting_injection or (param.klass and dependency_container.is_type_known(param.klass)):
-                return True
+        is_requesting_injection = isinstance(param.annotation, InjectableType)
+        if is_requesting_injection or (param.klass and dependency_container.is_type_known(param.klass)):
+            return True
 
     return False
