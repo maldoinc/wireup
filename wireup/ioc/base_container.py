7c3322d58544ea51bb9faf5b537bacb18b460330
Aldo Mateli <aldo.mateli@gmail.com>
2024-08-25 10:31:07 +0100

    Refactor container

--- wireup/ioc/base_container.py
+++ wireup/ioc/base_container.py
@@ -1,13 +1,23 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, TypeVar
 
+from wireup.errors import (
+    UnknownQualifiedServiceRequestedError,
+    UnknownServiceRequestedError,
+    UsageOfQualifierOnUnknownObjectError,
+)
 from wireup.ioc.override_manager import OverrideManager
+from wireup.ioc.types import EmptyContainerInjectionRequest, InjectableType, Qualifier
 
 if TYPE_CHECKING:
+    from collections.abc import Callable
+
     from wireup import ParameterBag
     from wireup.ioc.service_registry import ServiceRegistry
     from wireup.ioc.types import ContainerObjectIdentifier
+
+T = TypeVar("T")
 
 
 class BaseContainer:
@@ -34,3 +44,29 @@ class BaseContainer:
     def override(self) -> OverrideManager:
         """Override registered container services with new values."""
         return self._override_mgr
+
+    def _get_ctor(self, klass: type[T], qualifier: Qualifier | None) -> Callable[..., T] | None:
+        if ctor := self._registry.factory_functions.get((klass, qualifier)):
+            return ctor
+
+        if self._registry.is_interface_known(klass):
+            concrete_class = self._registry.interface_resolve_impl(klass, qualifier)
+            return self._get_ctor(concrete_class, qualifier)
+
+        if self._registry.is_impl_known(klass):
+            if not self._registry.is_impl_with_qualifier_known(klass, qualifier):
+                raise UnknownQualifiedServiceRequestedError(
+                    klass,
+                    qualifier,
+                    self._registry.known_impls[klass],
+                )
+            return klass
+
+        # When injecting dependencies and a qualifier is used, throw if it's being used on an unknown type.
+        # This prevents the default value from being used by the runtime.
+        # We don't actually want that to happen as the value is used only for hinting the container
+        # and all values should be supplied.
+        if qualifier:
+            raise UsageOfQualifierOnUnknownObjectError(qualifier)
+
+        return None
--- wireup/ioc/dependency_container.py
+++ wireup/ioc/dependency_container.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import asyncio
 import functools
 import sys
+import warnings
 from typing import TYPE_CHECKING, Any, TypeVar, overload
 
 from wireup.ioc.base_container import BaseContainer
@@ -14,16 +15,13 @@ else:
 
 from wireup.errors import (
     InvalidRegistrationTypeError,
-    UnknownQualifiedServiceRequestedError,
     UnknownServiceRequestedError,
-    UsageOfQualifierOnUnknownObjectError,
 )
 from wireup.ioc.service_registry import ServiceRegistry
 from wireup.ioc.types import (
     AnyCallable,
     ContainerObjectIdentifier,
     EmptyContainerInjectionRequest,
-    InjectableType,
     ParameterWrapper,
     Qualifier,
     ServiceLifetime,
@@ -81,13 +79,20 @@ class DependencyContainer(BaseContainer):
         if instance := self.__initialized_objects.get((klass, qualifier)):
             return instance  # type: ignore[no-any-return]
 
-        return self.__create_concrete_type(klass, qualifier)
+        if res := self.__get_instance(klass, qualifier):
+            return res
+
+        raise UnknownServiceRequestedError(klass)
 
     def abstract(self, klass: type[T]) -> type[T]:
         """Register a type as an interface.
 
         This type cannot be initialized directly and one of the components implementing this will be injected instead.
         """
+        warnings.warn(
+            "Using container.abstract is deprecated. Use '@abstract' instead",
+            stacklevel=2,
+        )
         self._registry.register_abstract(klass)
 
         return klass
@@ -125,6 +130,11 @@ class DependencyContainer(BaseContainer):
         * Use as a decorator with parameters to specify qualifier and lifetime, @container.register(qualifier=...).
         * Call it directly with @container.register(some_class_or_factory, qualifier=..., lifetime=...).
         """
+        warnings.warn(
+            "Using container.register is deprecated. Use '@service' instead",
+            stacklevel=2,
+        )
+
         # Allow register to be used either with or without arguments
         if obj is None:
 
@@ -205,7 +215,7 @@ class DependencyContainer(BaseContainer):
         for klass in sorter.static_order():
             for qualifier in self._registry.known_impls[klass]:
                 if (klass, qualifier) not in self.__initialized_objects:
-                    self.__create_concrete_type(klass, qualifier)
+                    self.get(klass, qualifier)
 
     def __callable_get_params_to_inject(self, fn: AnyCallable) -> dict[str, Any]:
         values_from_parameters: dict[str, Any] = {}
@@ -224,9 +234,14 @@ class DependencyContainer(BaseContainer):
             # Don't check here for none because as long as it exists in the bag, the value is good.
             elif isinstance(param.annotation, ParameterWrapper):
                 values_from_parameters[name] = self._params.get(param.annotation.param)
-            elif param.klass and (obj := self.__get_instance(param.klass, param.qualifier_value, param.annotation)):
+            elif param.klass and (obj := self.__get_instance(param.klass, param.qualifier_value)):
                 values_from_parameters[name] = obj
             else:
+                # Normally the container won't throw if it encounters a type it doesn't know about
+                # But if it's explicitly marked as to be injected then we need to throw.
+                if param.klass and isinstance(param.annotation, EmptyContainerInjectionRequest):
+                    raise UnknownServiceRequestedError(param.klass)
+
                 names_to_remove.add(name)
 
         # If autowiring, the container is assumed to be final, so unnecessary entries can be removed
@@ -236,51 +251,13 @@ class DependencyContainer(BaseContainer):
 
         return values_from_parameters
 
-    def __create_concrete_type(self, klass: type[T], qualifier: Qualifier | None) -> T:
-        """Create the real instances of dependencies. Additional dependencies they may have will be lazily created."""
-        obj_id = klass, qualifier
+    def __get_instance(self, klass: type[T], qualifier: Qualifier | None) -> T | None:
+        if ctor := self._get_ctor(klass=klass, qualifier=qualifier):
+            instance = ctor(**self.__callable_get_params_to_inject(ctor))
 
-        if fn := self._registry.factory_functions.get(obj_id):
-            instance = fn(**self.__callable_get_params_to_inject(fn))
-        else:
-            args = self.__callable_get_params_to_inject(klass)
-            instance = klass(**args)
+            if self._registry.is_impl_singleton(klass):
+                self.__initialized_objects[klass, qualifier] = instance
 
-        if self._registry.is_impl_singleton(klass):
-            self.__initialized_objects[obj_id] = instance
-
-        return instance  # type: ignore[no-any-return]
-
-    def __get_instance(
-        self, klass: type[T], qualifier: Qualifier | None, annotation: InjectableType | None = None
-    ) -> T | None:
-        if self._registry.is_impl_known_from_factory(klass, qualifier):
-            # Objects generated from factories do not have qualifiers
-            return self.__create_concrete_type(klass, None)
-
-        if self._registry.is_interface_known(klass):
-            concrete_class = self._registry.interface_resolve_impl(klass, qualifier)
-            return self.__create_concrete_type(concrete_class, qualifier)
-
-        if self._registry.is_impl_known(klass):
-            if not self._registry.is_impl_with_qualifier_known(klass, qualifier):
-                raise UnknownQualifiedServiceRequestedError(
-                    klass,
-                    qualifier,
-                    self._registry.known_impls[klass],
-                )
-            return self.__create_concrete_type(klass, qualifier)
-
-        # Normally the container won't throw if it encounters a type it doesn't know about
-        # But if it's explicitly marked as to be injected then we need to throw.
-        if isinstance(annotation, EmptyContainerInjectionRequest):
-            raise UnknownServiceRequestedError(klass)
-
-        # When injecting dependencies and a qualifier is used, throw if it's being used on an unknown type.
-        # This prevents the default value from being used by the runtime.
-        # We don't actually want that to happen as the value is used only for hinting the container
-        # and all values should be supplied.
-        if qualifier:
-            raise UsageOfQualifierOnUnknownObjectError(qualifier)
+            return instance
 
         return None
