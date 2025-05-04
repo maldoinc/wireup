9b82e8f521375968cf4891ed0a3525637a3e1efb
Aldo Mateli <aldo.mateli@gmail.com>
2025-05-04 17:52:12 +0100

    wip

--- wireup/integration/fastapi.py
+++ wireup/integration/fastapi.py
@@ -3,6 +3,7 @@ from contextvars import ContextVar
 from typing import (
     Any,
     AsyncIterator,
+    Awaitable,
     Callable,
     Dict,
     Iterator,
@@ -10,9 +11,10 @@ from typing import (
     Union,
 )
 
-from fastapi import FastAPI, Request, WebSocket
+from fastapi import FastAPI, Request, Response, WebSocket
 from fastapi.requests import HTTPConnection
 from fastapi.routing import APIRoute, APIWebSocketRoute
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from wireup import inject_from_container, service
 from wireup.errors import WireupError
@@ -61,13 +63,26 @@ def fastapi_websocket_factory() -> WebSocket:
         raise WireupError(msg) from e
 
 
+# Warn: Make sure the logic evolves with the _request_middleware function.
+async def _wireup_request_middleware(request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
+    token = current_request.set(request)
+    try:
+        async with request.app.state.wireup_container.enter_scope() as scoped_container:
+            request.state.wireup_container = scoped_container
+            return await call_next(request)
+    finally:
+        current_request.reset(token)
+
+
 def _inject_fastapi_route(
     *,
     container: AsyncContainer,
     target: AnyCallable,
     http_connection_param_name: str,
     is_http_connection_in_signature: bool,
+    add_custom_middleware: bool,
 ) -> AnyCallable:
+    # Warn: Make sure the logic evolves with the _wireup_request_middleware function.
     @contextlib.contextmanager
     def _request_middleware(
         scoped_container: Union[ScopedAsyncContainer, ScopedSyncContainer],
@@ -84,16 +99,20 @@ def _inject_fastapi_route(
         finally:
             current_request.reset(token)
 
-    return inject_from_container(container, middleware=_request_middleware)(target)
+    return inject_from_container(container, middleware=_request_middleware if add_custom_middleware else None)(target)
 
 
-def _inject_routes(container: AsyncContainer, app: FastAPI) -> None:
+def _inject_routes(container: AsyncContainer, app: FastAPI, *, add_custom_middleware: bool) -> None:
     for route in app.routes:
         if (
             isinstance(route, (APIRoute, APIWebSocketRoute))
             and route.dependant.call
             and is_callable_using_wireup_dependencies(route.dependant.call)
         ):
+            if isinstance(route, APIRoute) and not add_custom_middleware:
+                route.dependant.call = inject_from_container(container)(route.dependant.call)
+                continue
+
             is_http_connection_in_signature = route.dependant.http_connection_param_name is not None
 
             if not route.dependant.http_connection_param_name:
@@ -104,6 +123,9 @@ def _inject_routes(container: AsyncContainer, app: FastAPI) -> None:
                 target=route.dependant.call,
                 http_connection_param_name=route.dependant.http_connection_param_name,
                 is_http_connection_in_signature=is_http_connection_in_signature,
+                # For websocket routes we always add the middleware as the regular fastapi middleware
+                # only applies to http requests.
+                add_custom_middleware=add_custom_middleware or isinstance(route, APIWebSocketRoute),
             )
 
 
@@ -120,7 +142,7 @@ def _update_lifespan(container: AsyncContainer, app: FastAPI) -> None:
     app.router.lifespan_context = lifespan
 
 
-def setup(container: AsyncContainer, app: FastAPI) -> None:
+def setup(container: AsyncContainer, app: FastAPI, *, expose_container_in_middleware: bool = True) -> None:
     """Integrate Wireup with FastAPI.
 
     Setup performs the following:
@@ -130,8 +152,10 @@ def setup(container: AsyncContainer, app: FastAPI) -> None:
 
     For more details, visit: https://maldoinc.github.io/wireup/latest/integrations/fastapi/
     """
+    if expose_container_in_middleware:
+        app.add_middleware(BaseHTTPMiddleware, dispatch=_wireup_request_middleware)
     _update_lifespan(container, app)
-    _inject_routes(container, app)
+    _inject_routes(container, app, add_custom_middleware=not expose_container_in_middleware)
     app.state.wireup_container = container
 
 
