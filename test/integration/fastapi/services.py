c587ca6aa2b5ca0de21e26f94f35bbc5f7f250b6
Mehmet Giritli <mehmet.giritli@stfc.ac.uk>
2025-05-06 18:39:59 +0100

    Add test service

--- test/integration/fastapi/services.py
+++ test/integration/fastapi/services.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from fastapi import Request
+from fastapi import Request, WebSocket
 from wireup import service
 
 
@@ -8,3 +8,15 @@ from wireup import service
 @dataclass
 class ServiceUsingFastapiRequest:
     req: Request
+
+
+@dataclass
+@service(lifetime="scoped")
+class WebsocketInjectedGreeterService:
+    websocket: WebSocket
+
+    async def greet(self):
+        await self.websocket.accept()
+        data = await self.websocket.receive_text()
+        await self.websocket.send_text(f"Hello {data}")
+        await self.websocket.close()
