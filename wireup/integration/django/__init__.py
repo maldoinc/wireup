a9b76ea9b04dd455b88527173fa41a97bfddf6f9
MuriloScarpaSitonio <murilo.scarpa@gmail.com>
2025-12-10 11:09:26 -0300

    fix tox

--- wireup/integration/django/__init__.py
+++ wireup/integration/django/__init__.py
@@ -1,4 +1,3 @@
 from wireup.integration.django.apps import WireupSettings, get_app_container, get_request_container, wireup_middleware
-from wireup.integration.django.ninja import inject as ninja_inject
 
-__all__ = ["WireupSettings", "get_app_container", "get_request_container", "ninja_inject", "wireup_middleware"]
+__all__ = ["WireupSettings", "get_app_container", "get_request_container", "wireup_middleware"]
