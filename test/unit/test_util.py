f439dfeb57c3222225e995dea54bb9e360354bc6
MuriloScarpaSitonio <murilo.scarpa@gmail.com>
2025-12-10 21:14:43 -0300

    PR remarks

--- test/unit/test_util.py
+++ test/unit/test_util.py
@@ -6,7 +6,12 @@ import pytest
 from typing_extensions import Annotated
 from wireup import Inject
 from wireup.errors import WireupError
-from wireup.ioc.types import AnnotatedParameter, InjectableType, ParameterWrapper, ServiceQualifier
+from wireup.ioc.types import (
+    AnnotatedParameter,
+    InjectableType,
+    ParameterWrapper,
+    ServiceQualifier,
+)
 from wireup.ioc.util import (
     get_globals,
     param_get_annotation,
@@ -31,10 +36,16 @@ class TestUtilityFunctions(unittest.TestCase):
         ): ...
 
         params = inspect.signature(inner)
-        self.assertEqual(param_get_annotation(params.parameters["_a"], globalns=globals()), AnnotatedParameter(str, d1))
-        self.assertEqual(param_get_annotation(params.parameters["_b"], globalns=globals()), None)
         self.assertEqual(
-            param_get_annotation(params.parameters["_c"], globalns=globals()), AnnotatedParameter(str, None)
+            param_get_annotation(params.parameters["_a"], globalns=globals()),
+            AnnotatedParameter(str, d1),
+        )
+        self.assertEqual(
+            param_get_annotation(params.parameters["_b"], globalns=globals()), None
+        )
+        self.assertEqual(
+            param_get_annotation(params.parameters["_c"], globalns=globals()),
+            AnnotatedParameter(str, None),
         )
         self.assertEqual(
             param_get_annotation(params.parameters["_d"], globalns=globals()),
@@ -47,7 +58,9 @@ class TestUtilityFunctions(unittest.TestCase):
         self.assertIsNone(
             param_get_annotation(params.parameters["_f"], globalns=globals()),
         )
-        self.assertEqual(param_get_annotation(params.parameters["_g"], globalns=globals()), None)
+        self.assertEqual(
+            param_get_annotation(params.parameters["_g"], globalns=globals()), None
+        )
 
     def test_annotated_parameter_hash_equality(self):
         self.assertEqual(
@@ -73,44 +86,40 @@ def _sample_function():
     pass
 
 
-class TestGetGlobals:
-    def test_returns_globals_for_regular_function(self):
-        # GIVEN
-        func = _sample_function
+def test_returns_globals_for_regular_function():
+    # GIVEN
+    func = _sample_function
 
-        # WHEN
-        result = get_globals(func)
+    # WHEN
+    result = get_globals(func)
 
-        # THEN
-        assert result is _sample_function.__globals__
+    # THEN
+    assert result is _sample_function.__globals__
 
-    def test_returns_globals_for_class(self):
-        # GIVEN
-        cls = MyCustomClass
 
-        # WHEN
-        result = get_globals(cls)
+def test_returns_globals_for_class():
+    # GIVEN
+    cls = MyCustomClass
 
-        # THEN
-        assert "_sample_function" in result
-        assert "MyCustomClass" in result
+    # WHEN
+    result = get_globals(cls)
 
-    def test_unwraps_functools_partial(self):
-        # GIVEN
-        partial_func = functools.partial(_sample_function)
+    # THEN
+    assert "_sample_function" in result
+    assert "MyCustomClass" in result
 
-        # WHEN
-        result = get_globals(partial_func)
 
-        # THEN
-        assert result is _sample_function.__globals__
+@pytest.mark.parametrize(
+    "partial_func",
+    (
+        functools.partial(_sample_function),
+        functools.partial(functools.partial(functools.partial(_sample_function))),
+    ),
+)
+def test_unwraps_functools_partial(partial_func: functools.partial):
+    # GIVEN
+    # WHEN
+    result = get_globals(partial_func)
 
-    def test_unwraps_nested_functools_partial(self):
-        # GIVEN
-        partial_func = functools.partial(functools.partial(functools.partial(_sample_function)))
-
-        # WHEN
-        result = get_globals(partial_func)
-
-        # THEN
-        assert result is _sample_function.__globals__
+    # THEN
+    assert result is _sample_function.__globals__
