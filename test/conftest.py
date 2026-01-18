3609f11b30ab99980d9863b23d98cd2107abfbaf
Aldo Mateli <aldo.mateli@gmail.com>
2026-01-18 19:01:58 +0000

    Cleanup __init__.py

--- test/conftest.py
+++ test/conftest.py
@@ -2,10 +2,12 @@ from typing import Union
 
 import pytest
 import wireup
+from wireup.ioc.container.async_container import AsyncContainer
+from wireup.ioc.container.sync_container import SyncContainer
 
 from test.unit import services
 
-Container = Union[wireup.SyncContainer, wireup.AsyncContainer]
+Container = Union[SyncContainer, AsyncContainer]
 
 
 @pytest.fixture(params=[wireup.create_sync_container, wireup.create_async_container])
