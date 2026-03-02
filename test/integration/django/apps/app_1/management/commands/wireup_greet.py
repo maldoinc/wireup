62a2ef7dab207f1b23ce5038f5b75589db938116
Aldo Mateli <aldo.mateli@gmail.com>
2026-03-02 15:30:05 +0000

    fix lint

--- test/integration/django/apps/app_1/management/commands/wireup_greet.py
+++ test/integration/django/apps/app_1/management/commands/wireup_greet.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.core.management.base import BaseCommand
 from wireup import Injected
 from wireup.integration.django import inject_app
@@ -8,9 +10,9 @@ from test.shared.shared_services.greeter import GreeterService
 class Command(BaseCommand):
     help = "Greet using a Wireup-injected dependency."
 
-    def add_arguments(self, parser):
+    def add_arguments(self, parser: Any):
         parser.add_argument("--name", default="World")
 
     @inject_app
-    def handle(self, *args, name: str, greeter: Injected[GreeterService], **options):
+    def handle(self, *_args: Any, name: str, greeter: Injected[GreeterService], **_options: Any):
         self.stdout.write(greeter.greet(name))
