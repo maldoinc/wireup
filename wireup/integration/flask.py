45601711827af8aaff19d1531ab386720fe9440c
Simon Brunning <simon@brunningonline.net>
2026-03-23 08:10:18 +0000

    Add async version of flask setup.

--- wireup/integration/flask.py
+++ wireup/integration/flask.py
@@ -1,31 +1,41 @@
-from typing import Optional
+from __future__ import annotations
 
+from functools import singledispatch
+
+from asgiref.sync import async_to_sync
 from flask import Flask, g
 
 from wireup._decorators import inject_from_container
+from wireup.ioc.container.async_container import AsyncContainer, ScopedAsyncContainer
 from wireup.ioc.container.sync_container import ScopedSyncContainer, SyncContainer
 
 
-def _inject_views(container: SyncContainer, app: Flask) -> None:
+def _inject_views(container: SyncContainer | AsyncContainer, app: Flask) -> None:
     inject_scoped = inject_from_container(container, get_request_container)
 
     app.view_functions = {name: inject_scoped(view) for name, view in app.view_functions.items()}
 
 
-def setup(container: SyncContainer, app: Flask) -> None:
+@singledispatch
+def setup(container: SyncContainer | AsyncContainer, app: Flask) -> None:
     """Integrate Wireup with Flask.
 
     Setup performs the following:
     * Injects dependencies into Flask views.
     * Creates a new container scope for each request, with a scoped lifetime matching the request duration.
     """
+    msg = "Unsupported type"
+    raise NotImplementedError(msg)
 
+
+@setup.register(SyncContainer)
+def setup_sync(container: SyncContainer, app: Flask) -> None:
     def _before_request() -> None:
         ctx = container.enter_scope()
         g.wireup_container_ctx = ctx
         g.wireup_container = ctx.__enter__()
 
-    def _teardown_request(exc: Optional[BaseException] = None) -> None:
+    def _teardown_request(exc: BaseException | None = None) -> None:
         if ctx := getattr(g, "wireup_container_ctx", None):
             ctx.__exit__(type(exc) if exc else None, exc, exc.__traceback__ if exc else None)
 
@@ -36,11 +46,29 @@ def setup(container: SyncContainer, app: Flask) -> None:
     app.wireup_container = container  # type: ignore[reportAttributeAccessIssue]
 
 
-def get_app_container(app: Flask) -> SyncContainer:
+@setup.register(AsyncContainer)
+def setup_async(container: AsyncContainer, app: Flask) -> None:
+    def _before_request() -> None:
+        ctx = container.enter_scope()
+        g.wireup_container_ctx = ctx
+        g.wireup_container = async_to_sync(ctx.__aenter__)()
+
+    def _teardown_request(exc: BaseException | None = None) -> None:
+        if ctx := getattr(g, "wireup_container_ctx", None):
+            async_to_sync(ctx.__aexit__)(type(exc) if exc else None, exc, exc.__traceback__ if exc else None)
+
+    app.before_request(_before_request)
+    app.teardown_request(_teardown_request)
+
+    _inject_views(container, app)
+    app.wireup_container = container
+
+
+def get_app_container(app: Flask) -> SyncContainer | AsyncContainer:
     """Return the container associated with the given application."""
     return app.wireup_container  # type: ignore[reportAttributeAccessIssue]
 
 
-def get_request_container() -> ScopedSyncContainer:
+def get_request_container() -> ScopedSyncContainer | ScopedAsyncContainer:
     """Return the container handling the current request."""
     return g.wireup_container
